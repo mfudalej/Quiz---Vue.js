31f93dd61f6d34e9ac342329e8fa98df51d73981
Mateusz Fudalej <mateusz.fudalej@they.pl>
2019-08-25 22:41:54 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+            <b-nav-item
+                disabled
+            >
+<!--                Counter: 4/10-->
+                Counter: {{ numCorrect }}/{{ numTotal }}
+            </b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header.vue",
+        props: {
+            numCorrect: Number,
+            numTotal: Number
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
